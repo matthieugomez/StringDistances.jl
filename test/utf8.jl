fc6a85fb32638577f5aecda40a4c90893a67d40a
matthieugomez <gomez.matthieu@gmail.com>
2019-08-14 10:30:36 -0400

    Update utf8.jl

--- test/utf8.jl
+++ test/utf8.jl
@@ -7,3 +7,10 @@ compare(DamerauLevenshtein(), "aüa", "aua")
 compare(Hamming(), "aüa", "aua")
 compare(Jaro(), "aüa", "aua")
 compare(Levenshtein(), "aüa", "aua")
+
+
+s1 = "aü☃"
+s2 = "aüaüafs"
+dist = QGram(4)
+@test evaluate(dist, s1, s2) == 4
+
