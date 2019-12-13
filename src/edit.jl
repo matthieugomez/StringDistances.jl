2e99f85c214f03dc29aa4b4bbe29658ebdbd55ae
matthieugomez <gomez.matthieu@gmail.com>
2019-12-12 20:01:47 -0500

    clean

--- src/edit.jl
+++ src/edit.jl
@@ -247,3 +247,33 @@ function matching_blocks!(x::Set{Tuple{Int, Int, Int}}, s1::AbstractString, s2::
         len2 - (a[2] + a[3]) + 1, start1 + a[1] + a[3] - 1, start2 + a[2] + a[3] - 1)
     return x
 end
+
+# Return start of commn substring in s1, start of common substring in s2, and length of substring
+# Indexes refer to character number, not index
+function longest_common_substring(s1::AbstractString, s2::AbstractString, len1::Integer, len2::Integer)
+    if len1 > len2
+        start2, start1, len = longest_common_substring(s2, s1, len2, len1)
+    else
+        start1, start2, len = 0, 0, 0
+        p = zeros(Int, len2)
+        i1 = 0
+        for ch1 in s1
+            i1 += 1
+            i2 = 0
+            oldp = 0
+            for ch2 in s2
+                i2 += 1
+                newp = 0
+                if ch1 == ch2
+                    newp = oldp > 0 ? oldp : i2
+                    currentlength = (i2 - newp + 1)
+                    if currentlength > len
+                        start1, start2, len = i1 - currentlength + 1, newp, currentlength
+                    end
+                end
+                p[i2], oldp = newp, p[i2]
+            end
+        end
+    end
+    return start1, start2, len
+end
