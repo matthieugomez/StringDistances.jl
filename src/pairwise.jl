ff1daea00112e006b495f7ec2bfaec7e7abe562a
Robert Feldt <robert.feldt@gmail.com>
2020-11-07 12:23:10 +0100

    added pairwise for calculating distance matrices

--- src/pairwise.jl
+++ src/pairwise.jl
@@ -1,37 +1,56 @@
-_allocmat(X, Y, T) = Matrix{T}(undef, length(X), length(Y))
-_allocmat(X, T) = Matrix{T}(undef, length(X), length(X))
+_allocmatrix(X, Y, T) = Matrix{T}(undef, length(X), length(Y))
+_allocmatrix(X, T) = Matrix{T}(undef, length(X), length(X))
 
-pairwise(dist::StringDistance, X, Y; eltype = Float64) = 
-    pairwise!(_allocmat(X, Y, eltype), dist, X, Y)
+pairwise(dist::StringDistance, X, Y; eltype = Float64, precalc = nothing) =
+    pairwise!(_allocmatrix(X, Y, eltype), dist, X, Y; precalc)
 
-pairwise(dist::StringDistance, X; eltype = Float64) = 
-    pairwise!(_allocmat(X, eltype), dist, X)
+pairwise(dist::StringDistance, X; eltype = Float64, precalc = nothing) =
+    pairwise!(_allocmatrix(X, eltype), dist, X; precalc)
 
-function pairwise!(R::AbstractMatrix{N}, dist::StringDistance, X) where {N<:Number}
-    if dist isa SemiMetric
-        _symmetric_pairwise!(R, dist, X)
-    else
-        _asymmetric_pairwise!(R, dist, X, X)
-    end
-end
+pairwise!(R::AbstractMatrix{N}, dist::StringDistance, X; precalc = nothing) where {N<:Number} =
+    (dist isa SemiMetric) ?
+        _symmetric_pairwise!(R, dist, X; precalc) :
+        _asymmetric_pairwise!(R, dist, X, X; precalc)
 
-function pairwise!(R::AbstractMatrix{N}, dist::StringDistance, X, Y) where {N<:Number}
-    _asymmetric_pairwise!(R, dist, X, Y)
-end
+pairwise!(R::AbstractMatrix{N}, dist::StringDistance, X, Y; precalc = nothing) where {N<:Number} =
+    _asymmetric_pairwise!(R, dist, X, Y; precalc)
 
 _precalc(X, PT, q) = PT[PT(X[i], q) for i in 1:length(X)]
 
 const PrecalcMinLength = 5 # Only precalc if length >= 5
 
-function _symmetric_pairwise!(R, dist::QGramDistance, X; precalc = nothing, precalcType = QGramSortedVector)
-    # precalc if set to true or if isnothing and length is at least min length
-    shouldprecalc = (precalc === true) | (isnothing(precalc) & length(X) >= PrecalcMinLength)
-    objs = shouldprecalc ? _precalc(X, precalcType, q(dist)) : X
+function precalc_if_needed(X, dist::StringDistance, precalc, precalcType)
+    # precalc only if a QGramDistance and
+    # if precalc set to true or if isnothing and length is at least min length
+    !isa(dist, QGramDistance) && return X
+    cond = (precalc === true) ||
+                (isnothing(precalc) & length(X) >= PrecalcMinLength)
+    cond ? _precalc(X, precalcType, dist.q) : X
+end
+
+function _symmetric_pairwise!(R, dist::StringDistance, X;
+    precalc = nothing, precalcType = QGramSortedVector)
+
+    objs = precalc_if_needed(X, dist, precalc, precalcType)
 
     for i in 1:length(objs)
         R[i, i] = 0
-        for j in (i+1):length(objs)
+        Threads.@threads for j in (i+1):length(objs)
             R[i, j] = R[j, i] = evaluate(dist, objs[i], objs[j])
+        end
+    end
+    return R
+end
+
+function _asymmetric_pairwise!(R, dist::StringDistance, X, Y;
+    precalc = nothing, precalcType = QGramSortedVector)
+
+    objsX = precalc_if_needed(X, dist, precalc, precalcType)
+    objsY = precalc_if_needed(Y, dist, precalc, precalcType)
+
+    for i in 1:length(objsX)
+        Threads.@threads for j in 1:length(objsY)
+            R[i, j] = evaluate(dist, objsX[i], objsY[j])
         end
     end
     return R
