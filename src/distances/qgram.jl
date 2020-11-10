36cd8cbfc55a53710af5e23c97e0a5fbe32350b8
Robert Feldt <robert.feldt@gmail.com>
2020-11-10 10:35:05 +0100

    more detailed doc string for MorisitaOverlap

--- src/distances/qgram.jl
+++ src/distances/qgram.jl
@@ -385,6 +385,9 @@ calculate(d::Overlap, c::ThreeCounters{Int, Overlap}) =
 Creates a MorisitaOverlap distance, a general, statistical measure of
 dispersion which can also be used on dictionaries such as created
 from q-grams. See https://en.wikipedia.org/wiki/Morisita%27s_overlap_index
+This is more fine-grained than many of the other QGramDistances since
+it is based on the counts per q-gram rather than only which q-grams are
+in the strings.
 
 The distance corresponds to
 
