9168cb3df7e011f6757a06ff8695a846e53396cc
Matthieu Gomez <gomez.matthieu@gmail.com>
2021-12-02 14:18:56 -0500

    Update edit.jl

--- src/distances/edit.jl
+++ src/distances/edit.jl
@@ -173,11 +173,11 @@ It is the minimum number of operations (consisting of insertions,
 deletions or substitutions of a single character, or transposition of two adjacent characters)
 required to change one string into the other.
 
-The distance differs slightly from the Damerau-Levenshtein algorithm by imposing
-the restriction that no substring is edited more than once. So for example, "CA" to "ABC" has an edit
-distance of 2 by a complete application of Damerau-Levenshtein, but a distance of 3 by this method that
-uses the optimal string alignment algorithm. In particular, the restricted distance does not satisfy
-the triangle inequality.
+The distance differs slightly from the DamerauLevenshtein distance by imposing
+the restriction that no substring is edited more than once. So for example, "CA" to "ABC" has a distance
+of 3 by the OptimalStringAlignment distance but a distance of 2 by the DamerauLevenshtein distance.
+In contrast to the DamerauLevenshtein distance, the OptimalStringAlignment distance
+does not satisfy the triangle inequality.
 """
 struct OptimalStringAlignment <: StringSemiMetric end
 
